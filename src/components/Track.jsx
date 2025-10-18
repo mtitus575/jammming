325eb655469aebdcd138f91d860bdbbeb4dc3554
Martin <mjtitus06@gmail.com>
2025-10-18 10:59:26 +0100

    Styled the currentTrack with basic styling. Will need updating when data comes in to use props for images, music etc.

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -1,33 +1,47 @@
 import styles from "../styles/modules/SearchResults.module.css";
+import trackStyles from "../styles/modules/Track.module.css";
+import song from "../../../testSong.mp3";
 //icons:
 import { CiCircleRemove } from "react-icons/ci";
 import { FaShuffle } from "react-icons/fa6";
 import { IoIosSkipBackward } from "react-icons/io";
 import { FaPauseCircle } from "react-icons/fa";
-
 import { FaPlayCircle } from "react-icons/fa";
-
 import { IoIosSkipForward } from "react-icons/io";
 import { FaRepeat } from "react-icons/fa6";
 
 function Track({ playlist, currentTrack }) {
-//  currentTrack param will be used to play the active song when any song is being played.
-// Use this is the "currentTrack" class below
+  //  currentTrack param will be used to play the active song when any song is being played.
+  // Use this is the "currentTrack" class below
 
   return (
-    <section style={{ display: "none" }}>
-      <div className="currentTrack">
-        <section>
-          <FaShuffle />
-          <IoIosSkipBackward />
-          <FaPlayCircle />
-          <FaPauseCircle />
-          <IoIosSkipForward />
+    <section style={{ display: "none" }} className={trackStyles.trackCtn}>
+      <div className={trackStyles.activeTrack}>
+        <section className={trackStyles.controlsCtn}>
+          <FaShuffle className={trackStyles.icon} />
+          <IoIosSkipBackward className={trackStyles.icon} />
+          <FaPlayCircle
+            className={trackStyles.icon}
+            id={trackStyles.playIcon}
+          />
+          <FaPauseCircle
+            className={trackStyles.icon}
+            id={trackStyles.pauseIcon}
+          />
+          <IoIosSkipForward className={trackStyles.icon} />
           <FaRepeat />
         </section>
-        <h4>SongName Placeholder</h4>
-        <p>Place a progress bar here:</p>
-        <p>add time stamps here</p>
+        <section className={trackStyles.artist}>
+          <h5>SongName Placeholder</h5>
+          <p>Artist</p>
+        </section>
+        <input
+          type="range"
+          name="playProgress"
+          id="playProgress"
+          className={trackStyles.playProgress}
+        />
+        <audio src={song} controls></audio>
       </div>
       <div>
         <h5>Playlist</h5>
